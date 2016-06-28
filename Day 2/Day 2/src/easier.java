4121872ca051786b6fea59e77492548cc6cdd2df
TheKenCode <artist13kennyg13@gmail.com>
2016-06-28 09:03:43 -0700

    Day 2

---
+++ Day 2/Day 2/src/easier.java
@@ -0,0 +1,12 @@
+
+public class easier {
+	public static void print(Object a){
+		System.out.print(a);
+	}
+	public static void println(Object a){
+		System.out.println(a);
+	}
+	public static void println(){
+		System.out.println("");
+	}
+}
